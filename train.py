b18c5c8d10a6828457beb9e77a60ae4f07d7ccc8
supinfl <sdlymyleihongwei@163.com>
2024-03-03 12:53:52 +0000

    add read_file code

--- train.py
+++ train.py
@@ -1,6 +1,20 @@
 import pandas as pd
 
-def main():
-    return
+df = pd.read_csv('./original_data/train.csv')
 
-main()
+for index, row in df.iterrows():
+    eeg_id = row['eeg_id']
+    spectrogram_id = row['spectrogram_id']
+    
+    eeg_file_name = f"{eeg_id}.parquet"
+    spectrogram_file_name = f"{spectrogram_id}.parquet"
+    
+    eeg_file_path = f"./original_data/train_eegs/{eeg_file_name}"
+    spectrogram_file_path = f"./original_data/train_eegs/{spectrogram_file_name}"
+
+    eeg_df = pd.read_parquet(eeg_file_path)
+    spectrogram_df = pd.read_parquet(spectrogram_file_path)
+
+print(df.head())
+# print(eeg_df.head())
+# print(spectrogram_df.head())
