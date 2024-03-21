0f4ffe02c5bcf54a8c237dae651947658e16a6df
supinfl <sdlymyleihongwei@163.com>
2024-03-21 12:38:53 +0000

    add support code on logging model name and running time

--- read_parquet.py
+++ read_parquet.py
@@ -1,10 +1,20 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import time
+import datetime
 
-# Load the npy file
-# E:\PythonCode\Kaggle\TeamProject\UoS_HBAC\original_data\test_spectrograms\853520.npy
+# Function to write logs
+def write_log(model_name, run_time):
+    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    log_message = f"-------------------------\nTimestamp: {timestamp}\nModel: {model_name}\nRun Time: {run_time} seconds\n"
+    with open('run_log.txt', 'a') as file:
+        file.write(log_message)
 
+# Start the timer
+start_time = time.time()
+
+#Loading specific parquet file
 spec_id = 853520
 spec = pd.read_parquet(f'./original_data/test_spectrograms/{spec_id}.parquet')
 spec = spec.fillna(0).values[:, 1:].T # fill NaN values with 0, transpose for (Time, Freq) -> (Freq, Time)
@@ -30,3 +40,12 @@ plt.xlabel('Column Index')
 plt.ylabel('Value')
 plt.legend()
 plt.show()
+
+
+# End the timer
+end_time = time.time()
+run_time = end_time - start_time
+
+write_log("Baseline model", run_time)
+# Print out the run time
+print(f"The code ran for {run_time:.2f} seconds")
